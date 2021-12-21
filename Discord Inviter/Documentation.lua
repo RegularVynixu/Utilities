97f8d0cfcca4bcaf2f2e0d18b7740cf90d518b4c
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2021-12-21 13:25:40 +0100

    Update Documentation.lua

--- Discord Inviter/Documentation.lua
+++ Discord Inviter/Documentation.lua
@@ -6,7 +6,7 @@
     -- [i] Prompts an invite in-game, user can make the decision to join or not. This will also prompt on their Discord client.
     
     <void> function Inviter.Join(<string> invite)
-    -- [i] Skips the in-game prompt, but will still prompt on the user's Discord client.
+    -- [i] Skips the in-game prompt and sends a request to join the server, will still prompt on the user's Discord client.
 }
   
 return <table> Inviter
