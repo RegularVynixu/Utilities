02e53e58f7c1d42c420a0a2b4da7d3759a331ef6
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-04-02 11:39:24 +0200

    Create Documentation.lua

---
+++ Jailbreak/Documentation.lua
@@ -0,0 +1,9 @@
+<table> PuzzleLoader {
+    <void> function PuzzleLoader.new(<table> {
+        <table> Grid,
+        <table> Solution,
+    })
+    -- [!] This function is only compatible with Jailbreak's puzzle outputs
+}
+  
+return <table> PuzzleLoader
