93466259019638639ad06e286861110fdb08fa4b
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-06-25 20:48:51 +0200

    Delete Jailbreak/PuzzleLoader directory

--- Jailbreak/PuzzleLoader/Documentation.lua
+++
@@ -1,9 +0,0 @@
-<table> PuzzleLoader {
-    <void> function PuzzleLoader.new(<table> {
-        <table> Grid,
-        <table> Solution,
-    })
-    -- [!] This function is only compatible with Jailbreak's puzzle outputs
-}
-  
-return <table> PuzzleLoader
