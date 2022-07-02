bde7842f298e231ca4516456acee3043da95fe90
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-07-02 15:05:00 +0200

    Create Functions.lua

---
+++ Functions.lua
@@ -0,0 +1,5 @@
+return {
+    IsClosure = is_synapse_function or iskrnlclosure or isexecutorclosure,
+    SetIdentity = (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
+    Request = (syn and syn.request) or http_request or request
+}
