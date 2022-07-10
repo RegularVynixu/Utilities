c2214987b13bffeed4f9440943569eb9d5daee93
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-07-10 22:41:53 +0200

    Update Output.lua

--- Teleport Tool/Output.lua
+++ Teleport Tool/Output.lua
@@ -10,7 +10,7 @@ local function teleportToPoint(vec3, speed)
 		local diff = (vec3 - Root.Position)
 		Root = CFrame.new(Root.Position + diff.Unit * math.min(speed * step, diff.Magnitude))
 		
-		if (Vector3.new(vec3.X, 0, vec3.Z) - Vector3.new(Root.Position.X, 0, Root.Position.Z)) < 0.1 then
+		if (Vector3.new(vec3.X, 0, vec3.Z) - Vector3.new(Root.Position.X, 0, Root.Position.Z)).Magnitude < 0.1 then
 			Root.CFrame = CFrame.new(vec3); reached = true
 		end
 	end)
