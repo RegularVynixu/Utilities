589b0ae646aabbe877faa55804223a295dab448a
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-08-17 19:47:58 +0200

    Create Documentation.lua

---
+++ Jailbreak Notification/Documentation.lua
@@ -0,0 +1,11 @@
+-- Create A Notification
+
+<void> Notification.new(<table> data{
+    <string> Text,
+    <int> Duration, -- optional
+    <Color3> Color, -- optional
+})
+
+-- Set Color
+
+<void> Notification.setColor(<Color3> color)
