cbe30bb7af700a3505f3129348de39541bc73f7f
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-08-18 17:19:56 +0200

    Create Documentation.lua

---
+++ Jailbreak/CircleAction/Documentation.lua
@@ -0,0 +1,13 @@
+-- Create A Spec
+<function> CircleAction.Create(<table> data{
+    <string> Name,
+    <instance> Part,
+    <boolean> Timed,
+    <int> Duration,
+    <int> Dist,
+    <int> Priority,
+    <function> Callback,
+}) --[[ Returns a function which will destroy the created spec ]]--
+
+-- Destroy A Spec
+<bool> CircleAction.Remove(<table> spec) --[[ Returns a boolean depending on if destruction was successful ]]--
