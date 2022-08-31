f1db9f6b40667e87ba56f09dc7a6853c237b8643
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-08-31 11:06:42 +0200

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,13 +1,39 @@
-return {
+getgenv().StoredModules = { Names = {}, Objects = {} }
+
+local functions = {
     IsClosure = is_synapse_function or iskrnlclosure or isexecutorclosure,
     SetIdentity = (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
     Request = (syn and syn.request) or http_request or request,
     QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport,
     LoadModule = function(name)
-        for i, v in next, game:GetDescendants() do
-            if v.ClassName == "ModuleScript" and v.Name == name then
-                return require(v)
+        if #StoredModules == 0 then
+            for _, v in next, game:GetDescendants() do
+                if v.ClassName == "ModuleScript" then
+                    StoredModules.Names[#StoredModules.Names + 1] = v.Name
+                    StoredModules.Objects[#StoredModules.Objects + 1] = v
+                end
             end
+        end
+
+        local nameIdx = table.find(StoredModules.Names, name)
+
+        if not nameIdx then
+            for _, v in next, game:GetDescendants() do
+                if v.ClassName == "ModuleScript" and v.Name == name then
+                    StoredModules.Names[#StoredModules.Names + 1] = v.Name
+                    StoredModules.Objects[#StoredModules.Objects + 1] = v
+
+                    return require(v)
+                end
+            end
+        else
+            return require(StoredModules.Objects[nameIdx])
         end
     end,
 }
+
+for i, v in next, functions do
+    getgenv()[i] = v
+end
+
+return functions
