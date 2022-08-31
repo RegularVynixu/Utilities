e9ecc918210f27c53df14f4775f63d8745809e29
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-08-31 09:56:29 +0200

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,5 +1,13 @@
 return {
     IsClosure = is_synapse_function or iskrnlclosure or isexecutorclosure,
     SetIdentity = (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
-    Request = (syn and syn.request) or http_request or request
+    Request = (syn and syn.request) or http_request or request,
+    QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport,
+    LoadModule = function(name)
+        for i, v in next, game:GetDescendants() do
+            if v.ClassName == "ModuleScript" and v.Name == name then
+                return require(v)
+            end
+        end
+    end,
 }
