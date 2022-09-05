ee3a43942b3297e2b1cc23615ef4192b63e17310
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-09-05 20:55:06 +0200

    Update Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -5,6 +5,9 @@ local functions = {
     SetIdentity = (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
     Request = (syn and syn.request) or http_request or request,
     QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport,
+
+    -----
+    
     LoadModule = function(name)
         if #StoredModules == 0 then
             for _, v in next, game:GetDescendants() do
@@ -30,10 +33,19 @@ local functions = {
             return require(StoredModules.Objects[nameIdx])
         end
     end,
+    GetPlayerByName = function(name)
+        for _, v in next, game:GetService("Players"):GetPlayers() do
+            if string.find(string.lower(v.Name), string.lower(name)) or string.find(string.lower(v.DisplayName), string.lower(name)) then
+                return v
+            end
+        end
+    end,
 }
 
 for i, v in next, functions do
-    getgenv()[i] = v
+    if typeof(v) == "function" then
+        getgenv()[i] = v
+    end
 end
 
 return functions
