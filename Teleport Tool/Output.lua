a20a481d30123b79649a37827c79006da3f664e4
Paygammy <49263433+paygammy@users.noreply.github.com>
2022-09-14 22:06:52 -0700

    Type-checking (not Luau)

--- Teleport Tool/Output.lua
+++ Teleport Tool/Output.lua
@@ -1,24 +1,43 @@
-local Root = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-local RS = game:GetService("RunService")
+local Points = {}
 
-local function teleportToPoint(vec3, speed)
-	local bV = Instance.new("BodyVelocity")
-	bV.Velocity, bV.MaxForce = Vector3.new(), Vector3.new(9e9, 9e9, 9e9); bV.Parent = Root
+local NetworkClient = game:GetService("NetworkClient")
+local RunService = game:GetService("RunService")
+local LocalPlayer = NetworkClient:GetPlayer()
 
-	local reached, connection = false, nil
-	connection = RS.Stepped:Connect(function(_, step)
-		local diff = (vec3 - Root.Position)
-		Root.CFrame = CFrame.new(Root.Position + diff.Unit * math.min(speed * step, diff.Magnitude))
-		
-		if (Vector3.new(vec3.X, 0, vec3.Z) - Vector3.new(Root.Position.X, 0, Root.Position.Z)).Magnitude < 0.1 then
-			Root.CFrame = CFrame.new(vec3); reached = true
+local function TeleportToPoint(position, speed)
+	local p = position * Vector3.new(1, 0, 1)
+	if typeof(LocalPlayer) == 'Instance' then
+		local Character = LocalPlayer.Character
+		local Humanoid = typeof(Character) == 'Instance' and Character:FindFirstChildWhichIsA("Humanoid")
+		local HumanoidRootPart = typeof(Humanoid) == 'Instance' and Humanoid.RootPart
+		if typeof(HumanoidRootPart) == 'Instance' then
+			local BodyVelocity = Instance.new("BodyVelocity")
+			BodyVelocity.Velocity = Vector3.zero
+			BodyVelocity.MaxForce = Vector3.one * 9e9
+			BodyVelocity.Parent = HumanoidRootPart
+			
+			local reached = false
+			local connection; connection = RunService.Stepped:Connect(function(time, deltaTime)
+				local diff = (position - HumanoidRootPart.Position)
+				HumanoidRootPart.CFrame = CFrame.new(HumanoidRootPart.Position + diff.Unit * math.min(speed * deltaTime, diff.Magnitude))
+				if (p - (HumanoidRootPart.Position * Vector3.new(1, 0, 1))).Magnitude < .1 then
+					HumanoidRootPart.CFrame = CFrame.new(position)
+					reached = true
+				end
+			end)
+			while reached ~= true and typeof(connection) == 'RBXScriptConnection' and typeof(BodyVelocity) == 'Instance' do
+				RunService.Stepped:Wait()
+			end
+			if typeof(connection) == 'RBXScriptConnection' then
+				connection:Disconnect()
+			end
+			if typeof(BodyVelocity) == 'Instance' then
+				BodyVelocity:Destroy()
+			end
 		end
-	end)
-
-	repeat RS.RenderStepped:Wait() until reached == true
-	connection:Disconnect(); bV:Destroy()
+	end
 end
 
-for i, v in next, Points do
-	teleportToPoint(v, 25)
+for _, Point in pairs(Points) do
+	TeleportToPoint(Point, 25)
 end
