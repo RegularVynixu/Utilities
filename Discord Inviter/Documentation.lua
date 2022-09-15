54472f916caceee89773b69fb4813c3eb961d6b5
Paygammy <49263433+paygammy@users.noreply.github.com>
2022-09-14 22:17:31 -0700

    Update Documentation.lua

--- Discord Inviter/Documentation.lua
+++ Discord Inviter/Documentation.lua
@@ -1,12 +1,20 @@
-<table> Inviter {
-    <void> function Inviter.Prompt(<table> {
-        <string> invite,
-        <string> name, -- [optional] overwrites the displayed server name on the prompt
-    })
-    -- [i] Prompts an invite in-game, user can make the decision to join or not. This will also prompt on their Discord client.
-    
-    <void> function Inviter.Join(<string> invite)
-    -- [i] Skips the in-game prompt and sends a request to join the server, will still prompt on the user's Discord client.
+export type DiscordInvitationModule = {
+    -- print(#DiscordInvitationModule.Connections)
+    Connections: {
+        [number]: RBXScriptConnection
+    },
+    -- DiscordInvitationModule.Prompt({invite = "https://discord.gg/example", name = "Example Invite"})
+    Prompt: (
+        {
+            invite: string,
+            name: string?
+        }
+    ) -> (),
+    -- DiscordInvitationModule.Join("https://discord.gg/example")
+    Join: (invite: string) -> (),
+    -- print(DiscordInvitationModule.Gui:GetFullName())
+    Gui: ScreenGui
 }
-  
-return <table> Inviter
+
+--- The Discord invitation module, used to prompt invites to users.
+local DiscordInvitationModule: DiscordInvitationModule = require(...)
