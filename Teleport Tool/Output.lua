6e39e89f8394dc30a0e38d72dfb906becda08c92
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-09-16 19:49:23 +0200

    Update Output.lua

--- Teleport Tool/Output.lua
+++ Teleport Tool/Output.lua
@@ -5,20 +5,25 @@ local function teleportToPoint(vec3, speed)
 	local bV = Instance.new("BodyVelocity")
 	bV.Velocity, bV.MaxForce = Vector3.new(), Vector3.new(9e9, 9e9, 9e9); bV.Parent = Root
 
-	local reached, connection = false, nil
-	connection = RS.Stepped:Connect(function(_, step)
-		local diff = (vec3 - Root.Position)
+	local reached = false
+	local connection = RS.Stepped:Connect(function(_, step)
+		local diff = vec3 - Root.Position
+
 		Root.CFrame = CFrame.new(Root.Position + diff.Unit * math.min(speed * step, diff.Magnitude))
 		
 		if (Vector3.new(vec3.X, 0, vec3.Z) - Vector3.new(Root.Position.X, 0, Root.Position.Z)).Magnitude < 0.1 then
-			Root.CFrame = CFrame.new(vec3); reached = true
+			Root.CFrame = CFrame.new(vec3)
+            
+            reached = true
 		end
 	end)
 
-	repeat RS.RenderStepped:Wait() until reached == true
-	connection:Disconnect(); bV:Destroy()
+	repeat RS.RenderStepped:Wait() until reached
+
+	connection:Disconnect()
+    bV:Destroy()
 end
 
-for i, v in next, Points do
+for _, v in next, Points do
 	teleportToPoint(v, 25)
 end
