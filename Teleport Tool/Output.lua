695cddb5ef1d9ce84d1871517614f08244e1e685
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-09-16 20:08:42 +0200

    Update Output.lua

--- Teleport Tool/Output.lua
+++ Teleport Tool/Output.lua
@@ -1,14 +1,14 @@
-local Root = game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-local RS = game:GetService("RunService")
+local Player = game:GetService("Players").LocalPlayer
+local Char = Player.Character or Player.CharacterAdded:Wait()
+local Root = Char:WaitForChild("HumanoidRootPart")
 
 local function teleportToPoint(vec3, speed)
 	local bV = Instance.new("BodyVelocity")
 	bV.Velocity, bV.MaxForce = Vector3.new(), Vector3.new(9e9, 9e9, 9e9); bV.Parent = Root
 
 	local reached = false
-	local connection = RS.Stepped:Connect(function(_, step)
+	local connection = game:GetService("RunService").Stepped:Connect(function(_, step)
 		local diff = vec3 - Root.Position
-
 		Root.CFrame = CFrame.new(Root.Position + diff.Unit * math.min(speed * step, diff.Magnitude))
 		
 		if (Vector3.new(vec3.X, 0, vec3.Z) - Vector3.new(Root.Position.X, 0, Root.Position.Z)).Magnitude < 0.1 then
@@ -18,7 +18,7 @@ local function teleportToPoint(vec3, speed)
 		end
 	end)
 
-	repeat RS.RenderStepped:Wait() until reached
+	repeat task.wait() until reached
 
 	connection:Disconnect()
     bV:Destroy()
