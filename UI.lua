de8d3a050e99b0ef418b92c6c98de1b97c77df1c
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-10-28 09:03:51 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -8,28 +8,20 @@ local Players = game:GetService("Players")
 local Plr = Players.LocalPlayer
 local Mouse = Plr:GetMouse()
 
-local Exploit = {
-    Request = (syn and syn.request) or request or http_request,
-    GetAsset = getsynasset or getcustomasset
-}
-local Directory = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Directory.lua"))()
-
 local UI = {
     Color = {
         Add = function(c1, c2)
             local r, g, b = c1.R + c2.R, c1.G + c2.G, c1.B + c2.B
             return Color3.fromRGB(math.min(r * 255, 255), math.min(g * 255, 255), math.min(b * 255, 255))
         end,
-
         Sub = function(c1, c2)
             local r, g, b = c1.R - c2.R, c1.G - c2.G, c1.B - c2.B
             return Color3.fromRGB(math.max(r * 255, 0), math.max(g * 255, 0), math.max(b * 255, 0))
         end,
-
         ToFormat = function(color3)
             return "rgb(".. math.floor(math.min(color3.R * 255, 255)).. ", ".. math.floor(math.min(color3.G * 255, 255)).. ", ".. math.floor(math.min(color3.B * 255, 255)).. ")"
-        end
-    }
+        end,
+    },
 }
 
 -- Functions
@@ -76,32 +68,6 @@ UI.MakeDraggable = function(obj, drag, smoothness)
             TS:Create(obj, TweenInfo.new(math.clamp(smoothness, 0, 1), Enum.EasingStyle.Sine), { Position = UDim2.new(0, Mouse.X - startPos.X, 0, Mouse.Y - startPos.Y) }):Play()
         end
     end)
-end
-
-UI.LoadCustomAsset = function(imageUrl)
-    if Exploit.Request and Exploit.GetAsset then
-        local directory = Directory.Create({ "Loaded Assets" })
-
-        for i, v in next, listfiles(directory.Root) do
-            delfile(v)
-        end
-
-        --
-
-        local data = Exploit.Request({ Url = imageUrl, Method = "GET" }).Body
-        local fileName = directory.Root.. "/".. tick().. ".txt"
-        writefile(fileName, data)
-        
-        task.spawn(function()
-            task.wait(3)
-
-            if isfile(fileName) then
-                delfile(fileName)
-            end
-        end)
-        
-        return Exploit.GetAsset(fileName)
-    end
 end
 
 -- Scripts
