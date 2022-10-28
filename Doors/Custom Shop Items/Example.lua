04b5ce5777e4020cd0dae77f3da574a16fdecfca
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-10-29 00:59:35 +0200

    Update Example.lua

--- Doors/Custom Shop Items/Example.lua
+++ Doors/Custom Shop Items/Example.lua
@@ -4,16 +4,19 @@
 
 ]]--
 
+local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))()
 
+
+-- Create your tool here
+local exampleTool = LoadCustomInstance("rbxassetid://11397433017")
+
+
 -- Create custom shop item
-CustomShop:CreateItem({
+CustomShop.CreateItem(exampleTool, {
     Title = "Example Item",
     Desc = "Example description",
     Image = "https://cdn.discordapp.com/attachments/1034486774627573821/1035460240352747541/ExampleImage.png",
     Price = 999,
     Stack = 1,
-    ToolAssetId = "rbxassetid://11397433017", -- Your tool id here
-}, function(tool)
-    print(tool.Name.. " has been added into your inventory.")
-end)
+})
