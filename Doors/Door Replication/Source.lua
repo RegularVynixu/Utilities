207d21f04b73922b3e84792fa65c11c280eda114
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-11-11 13:50:41 +0100

    Update Source.lua

--- Doors/Door Replication/Source.lua
+++ Doors/Door Replication/Source.lua
@@ -24,47 +24,43 @@ local DoorReplication = {}
 -- Misc Functions
 
 local function openDoor(doorTable)
-    local model = doorTable.Model
-    local config = doorTable.Config
-    local debug = doorTable.Debug
+    doorTable.Debug.OnDoorPreOpened()
+    doorTable.Model:SetAttribute("Opened", true)
 
-    debug.OnDoorPreOpened()
-    model:SetAttribute("Opened", true)
-
-    if model:FindFirstChild("Lock") then
+    if doorTable.Model:FindFirstChild("Lock") then
         -- Unlock visual
 
-        model.Lock.UnlockPrompt.Enabled = false
-        model.Lock.M_Thing.C0 = model.Lock.M_Thing.C0 * CFrame.Angles(0, math.rad(-45), 0)
-        model.Hinge.Lock:Destroy()
-        model.Lock.UnlockPrompt:Destroy()
+        doorTable.Model.Lock.UnlockPrompt.Enabled = false
+        doorTable.Model.Lock.M_Thing.C0 = doorTable.Model.Lock.M_Thing.C0 * CFrame.Angles(0, math.rad(-45), 0)
+        doorTable.Model.Hinge.Lock:Destroy()
+        doorTable.Model.Lock.UnlockPrompt:Destroy()
     end
 
     -- Door opening visual
 
-    if model:FindFirstChild("Light") then
-        model.Light.Light.Color = Color3.fromRGB(197, 113, 88)
-        model.Light.Light.Attachment.PointLight.Enabled = true
-        model.Light.Light.Hit:Play()
+    if doorTable.Model:FindFirstChild("Light") then
+        doorTable.Model.Light.Light.Color = Color3.fromRGB(197, 113, 88)
+        doorTable.Model.Light.Light.Attachment.PointLight.Enabled = true
+        doorTable.Model.Light.Light.Hit:Play()
     end
     
-    model.Door.CanCollide = false
-    model.Door.Open:Play()
-    model.Hidden:Destroy()
+    doorTable.Model.Door.CanCollide = false
+    doorTable.Model.Door.Open:Play()
+    doorTable.Model.Hidden:Destroy()
 
     task.spawn(function()
-        local knobC1 = model.Hinge.Knob.C1
+        local knobC1 = doorTable.Model.Hinge.Knob.C1
 
-        TS:Create(model.Hinge.Knob, TweenInfo.new(0.15, Enum.EasingStyle.Quad), {C1 = knobC1 * CFrame.Angles(0, 0, math.rad(-35))}):Play()
+        TS:Create(doorTable.Model.Hinge.Knob, TweenInfo.new(0.15, Enum.EasingStyle.Quad), {C1 = knobC1 * CFrame.Angles(0, 0, math.rad(-35))}):Play()
         task.wait(0.15)
-        TS:Create(model.Hinge.Knob, TweenInfo.new(0.15, Enum.EasingStyle.Quad), {C1 = knobC1}):Play()
+        TS:Create(doorTable.Model.Hinge.Knob, TweenInfo.new(0.15, Enum.EasingStyle.Quad), {C1 = knobC1}):Play()
     end)
 
-    TS:Create(model.Hinge, TweenInfo.new(config.SlamOpen and 0.15 or 0.75, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {CFrame = model.Hinge.CFrame * CFrame.Angles(0, math.rad(-90), 0)}):Play()
+    TS:Create(doorTable.Model.Hinge, TweenInfo.new(doorTable.Config.SlamOpen and 0.15 or 0.75, Enum.EasingStyle.Quad, Enum.EasingDirection.In), {CFrame = doorTable.Model.Hinge.CFrame * CFrame.Angles(0, math.rad(-90), 0)}):Play()
 
     -- Next room preparations
 
-    local nextRoom = workspace.CurrentRooms:FindFirstChild(tonumber(model.Parent.Name) + 1)
+    local nextRoom = workspace.CurrentRooms:FindFirstChild(tonumber(doorTable.Model.Parent.Name) + 1)
 
     if nextRoom then
         for _, v in next, {"Assets", "Light_Fixtures"} do
@@ -78,57 +74,82 @@ local function openDoor(doorTable)
         end
     end
 
-    debug.OnDoorOpened()
+    doorTable.Debug.OnDoorOpened()
 end
 
 -- Functions
 
 DoorReplication.CreateDoor = function(config)
-    local door = Assets.Door:Clone()
-    door.Door.MaterialVariant = "PlywoodALT"
-    door.Sign.MaterialVariant = "Plywood"
+    -- Configs setup
+
+    for _, v in next, {"Key", "Lockpick"} do
+        if not table.find(config.CustomKeyNames, v) then
+            table.insert(config.CustomKeyNames, v)
+        end
+    end
+
+    -- Model
+
+    local model = Assets.Door:Clone()
+    model.Door.MaterialVariant = "PlywoodALT"
+    model.Sign.MaterialVariant = "Plywood"
 
     if not config.Barricaded then
-        door.Boards:Destroy()
+        model.Boards:Destroy()
         
         if not config.Locked then
-            door.Lock:Destroy()
+            model.Lock:Destroy()
         end
 
         if config.Sign == false then
-            door.Sign:Destroy()
-            door.Gui:Destroy()
+            model.Sign:Destroy()
+            model.Gui:Destroy()
         end
 
         if config.Light == false then
-            door.Light:Destroy()
+            model.Light:Destroy()
         end
     else
-        door.Lock:Destroy()
-        door.Sign:Destroy()
-        door.Gui:Destroy()
+        model.Lock:Destroy()
+        model.Sign:Destroy()
+        model.Gui:Destroy()
     end
-
-    return door
+    
+    return {
+        Model = model,
+        Config = config,
+        Debug = {
+            OnDoorPreOpened = function() end,
+            OnDoorOpened = function() end,
+        },
+    }
 end
 
 DoorReplication.Replicate = function(doorTable)
-    local model = doorTable.Model
-    local config = doorTable.Config
+    -- Pre-check
+
+    if not doorTable.Model.Parent then
+        warn("Failure - Parent the door before replicating it")
+        return
+    
+    elseif doorTable.Config.Barricaded then
+        warn("Failure - Attempt to replicate a barricaded door")
+        return
+    end
 
     -- Guiding light
 
-    if config.GuidingLight ~= false and model.Parent:GetAttribute("IsDark") then
+    if doorTable.Config.GuidingLight ~= false and doorTable.Model.Parent:GetAttribute("IsDark") then
         task.spawn(function()
-            if not door.Door.LightAttach.HelpLight.Enabled then
+            if not doorTable.Model.Door.LightAttach.HelpLight.Enabled then
                 task.wait(15)
             end
 
-            if model.Parent and not model:GetAttribute("Opened") then
-                model.Door.LightAttach.HelpLight.Enabled = true
-                model.Door.LightAttach.HelpParticle.Enabled = true
+            if doorTable.Model.Parent and not doorTable.Model:GetAttribute("Opened") then
+                doorTable.Model.Door.LightAttach.HelpLight.Enabled = true
+                doorTable.Model.Door.LightAttach.HelpParticle.Enabled = true
 
-                TS:Create(model.Door.LightAttach.HelpLight, TweenInfo.new(2), {Brightness = 0.5}):Play()
+                TS:Create(doorTable.Model.Door.LightAttach.HelpLight, TweenInfo.new(2), {Brightness = 0.5}):Play()
             end
         end)
     end
@@ -137,13 +158,14 @@ DoorReplication.Replicate = function(doorTable)
 
     local connections = {}
 
-    if model:FindFirstChild("Lock") then
-        connections.unlockBegan = model.Lock.UnlockPrompt.PromptButtonHoldBegan:Connect(function()
-            for _, v in next, config.CustomKeyNames do
+    if doorTable.Model:FindFirstChild("Lock") then
+        connections.unlockBegan = doorTable.Model.Lock.UnlockPrompt.PromptButtonHoldBegan:Connect(function()
+            for _, v in next, doorTable.Config.CustomKeyNames do
                 local key = Char:FindFirstChild(v)
 
                 if key and key:FindFirstChild("Animations") and key.Animations:FindFirstChild("use") then
                     Hum:LoadAnimation(key.Animations.use):Play()
+
                     return
                 end
             end
@@ -151,28 +173,30 @@ DoorReplication.Replicate = function(doorTable)
             firesignal(ReSt.Bricks.Caption.OnClientEvent, "You need a key!", true)
         end)
 
-        connections.unlockTriggered = model.Lock.UnlockPrompt.Triggered:Connect(function()
-            for _, v in next, connections do
-                v:Disconnect()
-            end
-
-            for _, v in next, config.CustomKeyNames do
+        connections.unlockTriggered = doorTable.Model.Lock.UnlockPrompt.Triggered:Connect(function()
+            for _, v in next, doorTable.Config.CustomKeyNames do
                 local key = Char:FindFirstChild(v)
 
                 if key then
-                    if config.DestroyKey ~= false then
+                    for _, v in next, connections do
+                        v:Disconnect()
+                    end
+
+                    if doorTable.Config.DestroyKey ~= false then
                         key:Destroy()
                     end
 
                     openDoor(doorTable)
+
                     break
                 end
             end
         end)
     else
-        while model.Parent and Root do
-            if (Root.Position - model.PrimaryPart.Position).Magnitude <= 15 then
+        while doorTable.Model.Parent and Root do
+            if (Root.Position - doorTable.Model.PrimaryPart.Position).Magnitude <= 15 then
                 openDoor(doorTable)
+
                 break
             end
 
@@ -181,63 +205,27 @@ DoorReplication.Replicate = function(doorTable)
     end
 end
 
-DoorReplication.ReplicateDoor = function(room, config)
-    -- Door table
+DoorReplication.ReplicateRoomDoor = function(room)
+    if room:FindFirstChild("Door") then
+        local originalDoor = room.Door
 
-    local doorTable = {
-        Config = config,
-        Debug = {
-            OnDoorPreOpened = function() end,
-            OnDoorOpened = function() end,
-        },
-    }
-
-    -- Pre-configs setup
-
-    for _, v in next, {"Key", "Lockpick"} do
-        if not table.find(config.CustomKeyNames, v) then
-            table.insert(config.CustomKeyNames, v)
-        end
-    end
-
-    -- Door model
-
-    local door = room:WaitForChild("Door", 1)
-
-    if door then
-        local repDoor = DoorReplication.CreateDoor({
-            Locked = room:WaitForChild("Assets"):WaitForChild("KeyObtain", 0.3) ~= nil,
+        local doorTable = DoorReplication.CreateDoor({
+            Locked = room:WaitForChild("Assets"):WaitForChild("KeyObtain", 0.3),
             Sign = true,
             Light = true,
             Barricaded = false,
+            CustomKeyNames = {},
+            DestroyKey = true,
+            GuidingLight = true,
+            SlamOpen = originalDoor:GetAttribute("FastOpen"),
         })
 
-        repDoor:SetPrimaryPartCFrame(door.PrimaryPart.CFrame)
-        repDoor.Parent = room
-        
-        doorTable.Model = repDoor
-
-        -- Door sign
-
-        local signText = ""
-        for _ = #tostring(room.Name + 1), 3 do
-            signText = signText.. "0"
-        end
-    
-        for _, v in next, repDoor.Gui:GetDescendants() do
-            if v.ClassName == "TextLabel" then
-                v.Text = signText.. tostring(room.Name + 1)
-            end
-        end
-
-        -- Handle replication
-
+        doorTable.Model:SetPrimaryPartCFrame(originalDoor.PrimaryPart.CFrame)
+        doorTable.Model.Parent = room
         DoorReplication.Replicate(doorTable)
-        door:Destroy()
-        
-        return doorTable
+        originalDoor:Destroy()
     else
-        warn("Failure - Could not find door in room:", room)
+        warn("Failure - Could not find original door of room:", room)
     end
 end
 
