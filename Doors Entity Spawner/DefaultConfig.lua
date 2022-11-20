8d4d006ef6d8243bbc07586a1153b12fb65d6bf7
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2022-11-20 09:20:57 +0100

    Create DefaultConfig.lua

---
+++ Doors Entity Spawner/DefaultConfig.lua
@@ -0,0 +1,28 @@
+return {
+    Speed = 100,
+    DelayTime = 2,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    BreakLights = true,
+    BackwardsMovement = false,
+    FlickerLights = {
+        true,
+        1
+    },
+    Cycles = {
+        Min = 1,
+        Max = 1,
+        WaitTime = 2
+    },
+    CamShake = {
+        true,
+        {5, 15, 0.1, 1},
+        100
+    },
+    Jumpscare = {
+        false,
+        {}
+    },
+    CustomDialog = {},
+}
