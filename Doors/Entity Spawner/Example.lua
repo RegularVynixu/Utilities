dbd215f5030c6deac222021324875de70720d1e5
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2023-02-27 09:58:06 +0100

    Update Example.lua

--- Doors/Entity Spawner/Example.lua
+++ Doors/Entity Spawner/Example.lua
@@ -1,7 +1,9 @@
--- Define the spawner
+---====== Define spawner ======---
+
 local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
 
--- Create an entity
+---====== Create entity ======---
+
 local entity = Spawner.createEntity({
     CustomName = "Template Entity",
     Model = "https://github.com/RegularVynixu/Utilities/blob/main/Doors/Entity%20Spawner/Assets/Entities/Rush.rbxm?raw=true", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
@@ -32,5 +34,44 @@ local entity = Spawner.createEntity({
     IsCuriousLight = false
 })
 
--- Run the entity
+---====== Debug ======---
+
+entity.Debug.OnEntitySpawned = function()
+    print("Entity has spawned")
+end
+
+entity.Debug.OnEntityDespawned = function()
+    print("Entity has despawned")
+end
+
+entity.Debug.OnEntityStartMoving = function()
+    print("Entity started moving")
+end
+
+entity.Debug.OnEntityFinishedRebound = function()
+    print("Entity finished rebound")
+end
+
+entity.Debug.OnEntityEnteredRoom = function(room)
+    print("Entity entered room:", room)
+end
+
+entity.Debug.OnLookAtEntity = function()
+    print("Player looking at entity")
+end
+
+entity.Debug.OnDeath = function()
+    print("Player has died")
+end
+
+--[[
+    NOTE: By overwriting 'OnUseCrucifix', the default crucifixion will be ignored and this function will be called instead
+
+    entity.Debug.OnUseCrucifix = function()
+        print("Custom crucifixion script here")
+    end
+]]--
+
+---====== Run entity ======---
+
 Spawner.runEntity(entity)
