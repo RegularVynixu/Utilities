0698de9c30b5611c02190c91374d14234e28f88c
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2023-04-15 18:53:58 +0200

    Create Source.lua

---
+++ Animation Player/Source.lua
@@ -0,0 +1,94 @@
+--[[ Services ]]--
+
+local tweenService = game:GetService("TweenService");
+
+--[[ Variables ]]--
+
+local animator = {};
+local storedMotors = {};
+
+--[[ Functions ]]--
+
+local function getKeyframesWithPose(animation, pose)
+	local keyframes = {};
+	local current;
+	local children = animation:GetChildren();
+	for i = 1, #children do
+		local keyframe = children[i];
+		if keyframe.ClassName == "Keyframe" then
+			if keyframe:FindFirstChild(pose.Name, true) then
+				keyframes[#keyframes + 1] = keyframe;
+			end
+			if pose:IsDescendantOf(keyframe) then
+				current = keyframe;
+			end
+		end
+	end
+	
+	table.sort(keyframes, function(a, b)
+		return a.Time < b.Time;
+	end);
+	
+	return keyframes, table.find(keyframes, current);
+end;
+
+local function findFirstDescendantOfClass(obj, class)
+	local descendants = obj:GetDescendants();
+	for i = 1, #descendants do
+		local desc = descendants[i];
+		if desc.ClassName == class then
+			return desc;
+		end
+	end
+end;
+
+animator.playAnimation = function(model, animation, speed)
+	local poses = {};
+	local descendants = animation:GetDescendants();
+	for i = 1, #descendants do
+		local pose = descendants[i];
+		if pose.ClassName == "Pose" then
+			local part = model:FindFirstChild(pose.Name, true);
+			if part then
+				local motor = part:FindFirstChildOfClass("Motor6D");
+				if motor then
+					storedMotors[motor.Name] = storedMotors[motor.Name] or motor.C0;
+					
+					local keyframes, index = getKeyframesWithPose(animation, pose);
+					local frame = keyframes[index];
+					local prevFrame = keyframes[index - 1];
+					local nextFrame = keyframes[index + 1];
+					
+					poses[#poses + 1] = {
+						time = frame.Time;
+						duration = prevFrame and frame.Time - prevFrame.Time or nextFrame and nextFrame.Time - frame.Time or 0;
+						style = Enum.EasingStyle[pose.EasingStyle.Name];
+						direction = Enum.EasingDirection[pose.EasingDirection.Name];
+						value = storedMotors[motor.Name] * pose.CFrame;
+						motor = motor;
+					};
+				end
+			end
+		end
+	end
+		
+	for _ = 1, animation.Loop and 9e9 or 1, 1 do
+		local passers = 0;
+		for i = 1, #poses do
+			passers += 1;
+			local pose = poses[i];
+			task.delay(pose.time, function()
+				local tween = tweenService:Create(pose.motor, TweenInfo.new(pose.duration, pose.style, pose.direction), {C0 = pose.value});
+				tween:Play();
+				tween.Completed:Wait()
+				passers -= 1;
+			end);
+		end
+		repeat task.wait() until passers <= 0;
+		break;
+	end
+end;
+
+--[[Main]]--
+
+return animator;
