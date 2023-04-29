fef1c34c699326422132ef0d8af80e78466ed2ca
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2023-04-29 20:12:52 +0200

    Update Source.lua

--- Doors Entity Spawner/Source.lua
+++ Doors Entity Spawner/Source.lua
@@ -282,7 +282,7 @@ Spawner.runEntity = function(entityTable)
                         ReSt.GameStats["Player_".. Plr.Name].Total.DeathCause.Value = entityModel.Name
                         
                         if #entityTable.Config.CustomDialog > 0 then
-                            firesignal(ReSt.Bricks.EntityInfo.OnClientEvent, entityTable.Config.CustomDialog, "Blue")
+                            firesignal(ReSt.EntityInfo.DeathHint.OnClientEvent, entityTable.Config.CustomDialog, "Blue")
                         end
                         
                         -- Unmute entity
