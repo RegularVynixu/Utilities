0e03c134094c53511276dd5630a4f6dc50bba50b
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-03-25 08:23:54 +0100

    Update Functions.lua
    the previously code sucked ass so he's an improved version

--- Functions.lua
+++ Functions.lua
@@ -1,113 +1,89 @@
 -- Services
-
 local Players = game:GetService("Players")
 
 -- Variables
+local localPlayer = Players.LocalPlayer
 
-local Plr = Players.LocalPlayer
-
-local Functions = {
-    IsClosure = is_synapse_function or iskrnlclosure or isexecutorclosure,
-    SetIdentity = (syn and syn.set_thread_identity) or set_thread_identity or setthreadidentity or setthreadcontext,
-    GetIdentity = (syn and syn.get_thread_identity) or get_thread_identity or getthreadidentity or getthreadcontext,
-    Request = (syn and syn.request) or http_request or request,
-    QueueOnTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport,
-    GetAsset = getsynasset or getcustomasset,
+local stuff = {
+    set_identity = set_thread_identity or setthreadidentity,
+    get_identity = get_thread_identity or getthreadidentity,
+    request = http_request or request
 }
-local ModuleScripts = {}
-
--- Misc Functions
-
-local function convertToAsset(str)
-    if isfile(str) then
-        return Functions.GetAsset(str)
-        
-    elseif str:find("rbxassetid") or tonumber(str) then
-        local numberId = str:gsub("%D", "")
-        return "rbxassetid://".. numberId
-        
-    elseif str:find("http") then
-        local req = Functions.Request({Url=str, Method="GET"})
-        
-        if req.Success then
-            local name = "customObject_".. tick().. ".txt"
-            writefile(name, req.Body)
-            return Functions.GetAsset(name)
-        end
-    end
-
-    return str
-end
+local moduleScripts = {}
 
 -- Functions
-
-Functions.GetPlayerByName = function(name)
-    for _, plr in next, Players:GetPlayers() do
+stuff.GetPlayerByName = function(name)
+    for _, plr in Players:GetPlayers() do
         if plr.Name:lower():find(name) or plr.DisplayName:lower():find(name) then
             return plr
         end
     end
 end
 
-Functions.LoadModule = function(name)
-    for _, ms in next, ModuleScripts do
+stuff.LoadModule = function(name)
+    for _, ms in moduleScripts do
         if ms.Name == name then
             return require(ms)
         end
     end
 end
 
-Functions.LoadCustomAsset = function(str)
-    if str == "" then
-        return ""
-    end
-
-    return convertToAsset(str)
-end
-
-Functions.LoadCustomInstance = function(str)
-    if str ~= "" then
-        local asset = convertToAsset(str)
-        local success, result = pcall(function()
-            return game:GetObjects(asset)[1]
-        end)
+stuff.LoadCustomAsset = function(path)
+    if path ~= "" then
+        if isfile(path) then
+            return getcustomasset(path, true)
     
-        if success then
-            return result
+        elseif path:find("rbxassetid") or tonumber(path) then
+            return "rbxassetid://".. path:match("%d+")
+    
+        elseif path:sub(1, 4) == "http" then
+            local r = request({
+                Url = path,
+                Method = "GET"
+            })
+            if r ~= nil and r.Success == true then
+                local fileName = ("customAsset_%s.txt"):format(tick())
+                writefile(fileName, r.Body)
+                local result = getcustomasset(fileName, true)
+                delfile(fileName)
+                return result
+            else
+                warn("Failed to load custom asset for: ".. path)
+            end
         end
     end
-
-    warn("Something went wrong attempting to load custom instance")
 end
 
--- Scripts
-
-for _, des in next, game:GetDescendants() do
-    if des.ClassName == "ModuleScript" then
-        table.insert(ModuleScripts, des)
+stuff.LoadCustomInstance = function(path)
+    if path ~= "" then
+        local success, result = pcall(function()
+            return game:GetObjects(LoadCustomAsset(path))[1]
+        end)
+        if success then
+            return result
+        else
+            warn("Failed to load custom instance for: ".. path)
+        end
     end
 end
 
-for name, func in next, Functions do
+-- Main
+for _, d in game:GetDescendants() do
+    if d.ClassName == "ModuleScript" then
+        moduleScripts[#moduleScripts + 1] = d
+    end
+end
+
+for name, func in stuff do
     if typeof(func) == "function" then
         getgenv()[name] = func
     end
 end
 
-game.DescendantAdded:Connect(function(des)
-    if des.ClassName == "ModuleScript" then
-        table.insert(ModuleScripts, des)
+game.DescendantAdded:Connect(function(d)
+    if d.ClassName == "ModuleScript" then
+        table.insert(moduleScripts, d)
     end
 end)
 
-Players.PlayerRemoving:Connect(function(player)
-    if player == Plr then
-        for _, file in next, listfiles("") do
-            if file:find("customObject") then
-                delfile(file)
-            end
-        end
-    end
-end)
-
-return Functions
+return stuff
