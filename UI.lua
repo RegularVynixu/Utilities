a7c8b0cc0d283ad70e34c7f2fb0abb60d9c5d1c4
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-03-25 09:06:07 +0100

    woohoo
    another module update, I am having a field day

--- UI.lua
+++ UI.lua
@@ -1,34 +1,37 @@
 -- Services
-
-local TS = game:GetService("TweenService")
+local TweenService = game:GetService("TweenService")
 local Players = game:GetService("Players")
 
 -- Variables
+local localPlayer = Players.LocalPlayer
+local localMouse = localPlayer:GetMouse()
 
-local Plr = Players.LocalPlayer
-local Mouse = Plr:GetMouse()
-
-local UI = {
+local ui = {
     Color = {
         Add = function(c1, c2)
-            local r, g, b = c1.R + c2.R, c1.G + c2.G, c1.B + c2.B
-            return Color3.fromRGB(math.min(r * 255, 255), math.min(g * 255, 255), math.min(b * 255, 255))
+            local r = math.min((c1.R + c2.R) * 255, 255)
+            local g = math.min((c1.G + c2.G) * 255, 255)
+            local b = math.min((c1.B + c2.B) * 255, 255)
+            return Color3.fromRGB(r, g, b)
         end,
         Sub = function(c1, c2)
-            local r, g, b = c1.R - c2.R, c1.G - c2.G, c1.B - c2.B
-            return Color3.fromRGB(math.max(r * 255, 0), math.max(g * 255, 0), math.max(b * 255, 0))
+            local r = math.max((c1.R - c2.R) * 255, 0)
+            local g = math.max((c1.G - c2.G) * 255, 0)
+            local b = math.max((c1.B - c2.B) * 255, 0)
+            return Color3.fromRGB(r, g, b)
         end,
-        ToFormat = function(color3)
-            return "rgb(".. math.floor(math.min(color3.R * 255, 255)).. ", ".. math.floor(math.min(color3.G * 255, 255)).. ", ".. math.floor(math.min(color3.B * 255, 255)).. ")"
-        end,
-    },
+        ToFormat = function(c)
+            local r = math.floor(math.min(c.R * 255, 255))
+            local g = math.floor(math.min(c.G * 255, 255))
+            local b = math.floor(math.min(c.B * 255, 255))
+            return ("rgb(%d, %d, %d)"):format(r, g, b)
+        end
+    }
 }
 
 -- Functions
-
-UI.Create = function(class, properties, radius)
+ui.Create = function(class, properties, radius)
 	local instance = Instance.new(class)
-
 	for i, v in next, properties do
 		if i ~= "Parent" then
 			if typeof(v) == "Instance" then
@@ -38,38 +41,33 @@ UI.Create = function(class, properties, radius)
 			end
 		end
 	end
-
 	if radius then
 		local uicorner = Instance.new("UICorner", instance)
 		uicorner.CornerRadius = radius
 	end
-    
 	return instance
 end
 
-UI.MakeDraggable = function(obj, drag, smoothness)
-    local startPos, dragging = nil, false
-
-    drag.InputBegan:Connect(function(input)
+ui.MakeDraggable = function(obj, dragObj, smoothness)
+    local startPos = nil
+    local dragging = false
+    dragObj.InputBegan:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseButton1 then
             dragging = true
-            startPos = Vector2.new(Mouse.X - obj.AbsolutePosition.X, Mouse.Y - obj.AbsolutePosition.Y)
+            startPos = Vector2.new(localMouse.X - obj.AbsolutePosition.X, localMouse.Y - obj.AbsolutePosition.Y)
         end
     end)
-
-    drag.InputEnded:Connect(function(input)
+    dragObj.InputEnded:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseButton1 then
             dragging = false
         end
     end)
-
-    Mouse.Move:Connect(function()
+    localMouse.Move:Connect(function()
         if dragging then
-            TS:Create(obj, TweenInfo.new(math.clamp(smoothness, 0, 1), Enum.EasingStyle.Sine), { Position = UDim2.new(0, Mouse.X - startPos.X, 0, Mouse.Y - startPos.Y) }):Play()
+            TweenService:Create(obj, TweenInfo.new(math.clamp(smoothness, 0, 1), Enum.EasingStyle.Sine), { Position = UDim2.new(0, localMouse.X - startPos.X, 0, localMouse.Y - startPos.Y) }):Play()
         end
     end)
 end
 
--- Scripts
-
-return UI
+-- Main
+return ui
