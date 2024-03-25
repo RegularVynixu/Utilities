8635bc7f5bfe0decc2a446c434b80876d4a348fd
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-03-25 08:57:35 +0100

    yay
    less ass now I guess I still don't know why I made this to beginw ith

--- Directory.lua
+++ Directory.lua
@@ -1,81 +1,110 @@
 -- Services
-
-local HS = game:GetService("HttpService")
+local HttpService = game:GetService("HttpService")
 
 -- Variables
-
-local Directory = {}
+local directory = {}
 
 -- Misc Functions
-
-local function createFolder(directory)
-    if not isfolder(directory) then
-        makefolder(directory)
+local function createFolder(path)
+    if not isfolder(path) then
+        makefolder(path)
     end
 end
 
-local function createBranch(tree, branch, directory)
-    for i, v in next, branch do
-        local branchName = typeof(v) == "table" and i or v
-        local branchDirectory = (directory or "").. "/".. branchName
+local function createBranch(tree, branch, path)
+    for i, v in branch do
+        if type(v) == "table" then
+            createBranch(tree, v, path .. "/" .. i)
+        else
+            createFolder(path .. "/" .. i)
+        end
+    end
+end
+
+-- Functions
+directory.Create = function(tree, fPath)
+    fPath = fPath or ""
+    local t = {}
+    for i, v in tree do
+        if type(v) == "table" then
+            createBranch(t, v, fPath .. "/" .. i)
+        else
+            createFolder(fPath .. "/" .. i)
+        end
+    end
+    return t
+end
+
+-- Services
+local HttpService = game:GetService("HttpService")
+
+-- Variables
+local directory = {}
+
+-- Misc Functions
+
+local function createFolder(path)
+    if not isfolder(path) then
+        makefolder(path)
+    end
+end
+
+local function createBranch(tree, branch, bPath)
+    for i, v in branch do
+        local name = (typeof(v) == "table") and i or v
+        bPath = (bPath or "").."/"..name
         
-        createFolder(branchDirectory)
-        tree[branchName] = branchDirectory
+        createFolder(bPath)
+        tree[name] = bPath
         
         if typeof(v) == "table" then
-            createBranch(tree, v, branchDirectory)
+            createBranch(tree, v, bPath)
         end
     end
 end
 
 -- Functions
 
-Directory.Create = function(tree, directory)
-    local newTree = {}
-
-    for i, v in next, tree do
-        local treeDirectory = (directory and directory.. "/" or "").. (typeof(v) == "table" and i or v)
-        
-        createFolder(treeDirectory)
-        newTree.Root = treeDirectory
-        
+directory.Create = function(tree, fPath)
+    fPath = fPath and fPath.."/" or ""
+    local t = {}
+    for i, v in tree do
+        local tPath = fPath..((typeof(v) == "table") and i or v)
+        t.Root = tPath
+        createFolder(tPath)
         if typeof(v) == "table" then
-            createBranch(newTree, v, treeDirectory)
+            createBranch(t, v, tPath)
         end
+        break
     end
-
-    return newTree
+    return t
 end
 
-Directory.WriteConfig = function(directory, data)
+directory.WriteConfig = function(fPath, content)
     local success = pcall(function()
-        writefile(directory, HS:JSONEncode(data))
+        writefile(fPath, HttpService:JSONEncode(content))
     end)
-
     return success
 end
 
-Directory.DecodeConfig = function(directory)
-    local success, dConfig = pcall(function()
-        return HS:JSONDecode(readfile(directory))
+directory.DecodeConfig = function(fPath)
+    local success, result = pcall(function()
+        return HttpService:JSONDecode(readfile(directory))
     end)
-
-    return success, success and dConfig or {}
+    return success, (success and result or {})
 end
 
-Directory.GetNameFromDirectory = function(directory, noExtension)
-    local splt = directory:split([[\]])
+directory.GetNameFromDirectory = function(fPath, noExtension)
+    local splt = fPath:split([[\]])
     local name = splt[#splt]
     
     if noExtension then
         local splt2 = name:split(".")
-        
         if #splt2 > 1 then
             name = splt2[1]
         end
     end
-    
     return name
 end
 
-return Directory
+return directory
