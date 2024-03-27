fac2153dea455407dbe82eabd4a30d5a414db0d9
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-03-27 07:57:19 +0100

    whoopsy poopsy
    forgot to update the loadstring url 🙏

--- Doors/Entity Spawner/V1/Example.lua
+++ Doors/Entity Spawner/V1/Example.lua
@@ -1,6 +1,6 @@
 ---====== Define spawner ======---
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
+local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
 
 ---====== Create entity ======---
 
