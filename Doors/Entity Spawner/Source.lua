cc39dbc018e2fe862aa849edf4e05a0d0e53f635
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-03-27 08:00:07 +0100

    do I really have to give each commit a message? seems a bit excessive does it not? how long can I make this message lmao it seems to go on forever and ever, do I just keep typing wtf? ok I am giving up now cya and enjoy the spawner! make sure to use the updated v2 when it releases I put a lot of time and effort into it, thank you very much dear user! much love.

--- Doors/Entity Spawner/Source.lua
+++ Doors/Entity Spawner/Source.lua
@@ -1,2 +1,2 @@
--- New directory yay!
+-- New directory for v1.0 Entity Spawner yay!
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V1/Source.lua"))()
