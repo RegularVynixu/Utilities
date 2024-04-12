b86deba343acd88e6c7a59b38718c3c5535eb7e5
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-04-12 18:32:47 +0200

    updated

--- Doors/Custom Achievements/Example.lua
+++ Doors/Custom Achievements/Example.lua
@@ -1,15 +1,10 @@
---[[
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
 
-    NOTE: Keep in mind that these are "fake" achievements and will NOT give you an in-game badge.
-
-]]--
-
-local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
-
--- Creates and displays your custom achievement
-Achievements.Get({
-    Title = "Example Achievement",
-    Desc = "This is very cool.",
-    Reason = "You executed the example script.",
-    Image = "https://images.emojiterra.com/twitter/v13.1/512px/1f913.png",
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Title",
+    Desc = "Description",
+    Reason = "Reason",
+    Image = "rbxassetid://12309073114"
 })
