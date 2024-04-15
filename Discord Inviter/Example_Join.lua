98f89b7aca70052d4d92165869d29cc4e559afde
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-04-15 16:52:59 +0200

    Create Example_Join.lua

---
+++ Discord Inviter/Example_Join.lua
@@ -0,0 +1,5 @@
+---====== Load Discord inviter ======---
+local discordInviter = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Discord%20Inviter/Source.lua"))()
+
+---====== Join Discord server ======---
+discordInviter.Join("https://discord.com/invite/roblox")
