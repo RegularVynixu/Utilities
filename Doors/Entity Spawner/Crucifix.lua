042ffc61505bf7745b80043658a259e67fa979ba
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-04-19 22:29:42 +0200

    simple crucifix giver for entity spawner

---
+++ Doors/Entity Spawner/Crucifix.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+LoadCustomInstance("https://github.com/RegularVynixu/Utilities/raw/main/Doors/Item%20Spawner/Assets/Crucifix.rbxm").Parent = game:GetService("Players").LocalPlayer.Backpack
