3a41f5e9d9c8bcc399230d1f05700777248fd4bf
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-05-03 22:35:50 +0200

    Update Functions.lua
    + GetGameLastUpdate : string
    + HasGameUpdated : boolean
    + TruncateNumber : number

--- Functions.lua
+++ Functions.lua
@@ -1,8 +1,11 @@
+-- Services
+local MarketplaceService = game:GetService("MarketplaceService")
+
 -- Variables
 local module = {}
 
 -- Functions
-module.LoadCustomAsset = function(url)
+module.LoadCustomAsset = function(url: string)
     if getcustomasset then
         if isfile(url) then
             return getcustomasset(url, true)
@@ -22,13 +25,30 @@ module.LoadCustomAsset = function(url)
     warn("Failed to load custom asset for:\n"..url)
 end
 
-module.LoadCustomInstance = function(url)
+module.LoadCustomInstance = function(url: string)
     local success, result = pcall(function()
         return game:GetObjects(module.LoadCustomAsset(url))[1]
     end)
     if success then
         return result
     end
+end
+
+module.GetGameLastUpdate = function()
+    return DateTime.fromIsoDate(MarketplaceService:GetProductInfo(game.PlaceId).Updated)
+end
+
+module.HasGameUpdated = function(timestamp: string | number | DateTime)
+    local millis = (typeof(timestamp) == "string" and DateTime.fromIsoDate(timestamp).UnixTimestampMillis) or (typeof(timestamp) == "number" and timestamp) or timestamp.UnixTimestampMillis
+    if millis then
+        return millis < module.GetGameLastUpdate().UnixTimestampMillis
+    end
+    return false
+end
+
+module.TruncateNumber = function(num: number, decimals: number)
+    local shift = 10 ^ (decimals and math.max(decimals, 0) or 0)
+	return num * shift // 1 / shift
 end
 
 -- Main
