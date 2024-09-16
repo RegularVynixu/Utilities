d3a86a89478596c1aab956b616982f309d07b5eb
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2024-09-16 17:23:05 +0200

    swapped checks for LoadCustomAsset
    This in an attempt to fix issue with Synapse Z checking against external links and outputting errors.

--- Functions.lua
+++ Functions.lua
@@ -14,14 +14,15 @@ end
 
 module.LoadCustomAsset = function(url: string)
     if getcustomasset then
-        if isfile(url) then
-            return getcustomasset(url, true)
-        elseif url:lower():sub(1, 4) == "http" then
+        if url:lower():sub(1, 4) == "http" then
             local fileName = `temp_{tick()}.txt`
             writefile(fileName, game:HttpGet(url))
             local result = getcustomasset(fileName, true)
             delfile(fileName)
             return result
+
+	elseif isfile(url) then
+            return getcustomasset(url, true)
         end
     else
         warn("Executor doesn't support 'getcustomasset', rbxassetid only.")
