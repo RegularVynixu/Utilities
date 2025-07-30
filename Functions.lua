004316facc65366a10b5f1b06746a8c266e6a189
Vynixu <60541419+RegularVynixu@users.noreply.github.com>
2025-07-30 11:20:05 +0200

    Update Functions.lua
    Added 'Require'

--- Functions.lua
+++ Functions.lua
@@ -3,17 +3,32 @@
 -- Services
 local MarketplaceService = game:GetService("MarketplaceService")
 local HttpService = game:GetService("HttpService")
-local Players = game:GetService("Players")
 
 -- Variables
-local module = {}
+local Module = {}
 
 -- Functions
 local function timestampToMillis(timestamp: string | number | DateTime)
     return (typeof(timestamp) == "string" and DateTime.fromIsoDate(timestamp).UnixTimestampMillis) or (typeof(timestamp) == "number" and timestamp) or timestamp.UnixTimestampMillis
 end
 
-module.LoadCustomAsset = function(url: string)
+Module.Require = function(s: string): any?
+    local content;
+    if s:lower():sub(1, 4) == "http" then
+        content = game:HttpGet(s)
+    elseif isfile(s) then
+        content = readfile(s)
+    end
+    if content then
+        local func, err = loadstring(content)
+        if not func then
+            error(debug.traceback("Failed to load module:\n"..s.."\nError: "..err))
+        end
+        return func()
+    end
+end
+
+Module.LoadCustomAsset = function(url: string): string?
     if getcustomasset then
         if url:lower():sub(1, 4) == "http" then
             local fileName = `temp_{tick()}.txt`
@@ -21,10 +36,9 @@ module.LoadCustomAsset = function(url: string)
             local result = getcustomasset(fileName, true)
             delfile(fileName)
             return result
-
-	elseif isfile(url) then
-            return getcustomasset(url, true)
         end
+	elseif isfile(url) then
+        return getcustomasset(url, true)
     else
         warn("Executor doesn't support 'getcustomasset', rbxassetid only.")
     end
@@ -34,54 +48,52 @@ module.LoadCustomAsset = function(url: string)
     error(debug.traceback("Failed to load custom asset for:\n"..url))
 end
 
-module.LoadCustomInstance = function(url: string)
-    local success, result = pcall(function()
-        return game:GetObjects(module.LoadCustomAsset(url))[1]
+Module.LoadCustomInstance = function(url: string): Instance?
+    local s, r = pcall(function()
+        return game:GetObjects(Module.LoadCustomAsset(url))[1]
     end)
-    if success then
-        return result
-    end
+    return s and r or nil
 end
 
-module.GetGameLastUpdate = function()
+Module.GetGameLastUpdate = function(): DateTime
     return DateTime.fromIsoDate(MarketplaceService:GetProductInfo(game.PlaceId).Updated)
 end
 
-module.HasGameUpdated = function(timestamp: string | number | DateTime)
+Module.HasGameUpdated = function(timestamp: string | number | DateTime): boolean
     local millis = timestampToMillis(timestamp)
     if millis then
-        return millis < module.GetGameLastUpdate().UnixTimestampMillis
+        return millis < Module.GetGameLastUpdate().UnixTimestampMillis
     end
     return false
 end
 
-module.GetGitLastUpdate = function(owner: string, repo: string, filePath: string)
+Module.GetGitLastUpdate = function(owner: string, repo: string, filePath: string): DateTime
     local url = `https://api.github.com/repos/{owner}/{repo}/commits?per_page=1&path={filePath}`
-    local success, result = pcall(HttpService.JSONDecode, HttpService, game:HttpGet(url))
-    if not success then
+    local s, r = pcall(HttpService.JSONDecode, HttpService, game:HttpGet(url))
+    if not s then
         error(debug.traceback("Failed to get last commit for:\n"..url))
     end
-    return DateTime.fromIsoDate(result[1].commit.committer.date)
+    return DateTime.fromIsoDate(r[1].commit.committer.date)
 end
 
-module.HasGitUpdated = function(owner: string, repo: string, filePath: string, timestamp: string | number | DateTime)
+Module.HasGitUpdated = function(owner: string, repo: string, filePath: string, timestamp: string | number | DateTime): boolean
     local millis = timestampToMillis(timestamp)
     if millis then
-        return millis < module.GetGitLastUpdate(owner, repo, filePath).UnixTimestampMillis
+        return millis < Module.GetGitLastUpdate(owner, repo, filePath).UnixTimestampMillis
     end
     return false
 end
 
-module.TruncateNumber = function(num: number, decimals: number)
+Module.TruncateNumber = function(num: number, decimals: number): number
     local shift = 10 ^ (decimals and math.max(decimals, 0) or 0)
-	return num * shift // 1 / shift
+    return num * shift // 1 / shift
 end
 
 -- Main
-for name, func in module do
+for name, func in next, Module do
     if typeof(func) == "function" then
         getgenv()[name] = func
     end
 end
 
-return module
+return Module
