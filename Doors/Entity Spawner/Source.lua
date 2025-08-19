22dbdc8354cdf3ec4d95918d1521d99e8e91eccd
FOCUS <focus_114514@qq.com>
2025-08-19 16:58:03 +0800

    Update Source.lua

--- Doors/Entity Spawner/Source.lua
+++ Doors/Entity Spawner/Source.lua
@@ -1,4 +1,4 @@
--- Entity Spawner V2 Fixed is Coming! 😱😱😱
+-- Entity Spawner V2 Fixed is here! 😱😱😱
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Focuslol666/Utilities/refs/heads/patch-1/Doors/Entity%20Spawner/V2/Source.lua"))()
 
 -- New directory for v1.0 Entity Spawner yay! (Abandoned 💀)
