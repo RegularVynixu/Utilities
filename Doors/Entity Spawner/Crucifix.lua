68babc0ab57f534516cc0e87ce29582f157798f9
FOCUS <focus_114514@qq.com>
2025-08-20 22:07:09 +0800

    Update Crucifix.lua

--- Doors/Entity Spawner/Crucifix.lua
+++ Doors/Entity Spawner/Crucifix.lua
@@ -4,5 +4,4 @@ local backpack = player.Backpack
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 local crucifix = LoadCustomInstance("https://github.com/RegularVynixu/Utilities/raw/main/Doors/Item%20Spawner/Assets/Crucifix.rbxm")
-crucifix.Name = "LocalCrucifix"
 crucifix.Parent = backpack
