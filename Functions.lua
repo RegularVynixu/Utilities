aa2d1755f15b82fb027bf63a1891c6ea4504a4a5
RegularVynixu <60541419+RegularVynixu@users.noreply.github.com>
2026-03-16 22:47:54 +0100

    ref(functions): refactored module

--- Functions.lua
+++ Functions.lua
@@ -1,40 +1,51 @@
 -- Additional help: @ActualMasterOogway
 
--- Services
+-- \\ Services // --
+
 local MarketplaceService = game:GetService("MarketplaceService")
 local HttpService = game:GetService("HttpService")
 
--- Variables
+-- \\ Variables // --
+
 local Module = {}
 
--- Functions
-local function timestampToMillis(timestamp: string | number | DateTime)
-    return (typeof(timestamp) == "string" and DateTime.fromIsoDate(timestamp).UnixTimestampMillis) or (typeof(timestamp) == "number" and timestamp) or timestamp.UnixTimestampMillis
+-- \\ Functions // --
+
+local function timestampToMillis(timestamp: string | number | DateTime): number
+    return
+        (typeof(timestamp) == "string" and DateTime.fromIsoDate(timestamp).UnixTimestampMillis)
+        or (typeof(timestamp) == "number" and timestamp)
+        or timestamp.UnixTimestampMillis
 end
 
+-- \\ Main // --
+
 Module.Require = function(s: string): any?
-    local content;
+    local content = s
     if s:lower():sub(1, 4) == "http" then
         content = game:HttpGet(s)
     elseif isfile(s) then
         content = readfile(s)
     end
-    if content then
-        local func, err = loadstring(content)
-        if not func then
-            error(debug.traceback("Failed to load module:\n"..s.."\nError: "..err))
-        end
-        return func()
+
+    local func, err = loadstring(content)
+    if not func then
+        error(debug.traceback("Failed to load module:\n"..s.."\nError: "..err))
     end
+    return func()
 end
 
 Module.LoadCustomAsset = function(url: string): string?
     if getcustomasset then
         if url:lower():sub(1, 4) == "http" then
             local fileName = `temp_{tick()}.txt`
-            writefile(fileName, game:HttpGet(url))
-            local result = getcustomasset(fileName, true)
-            delfile(fileName)
+            local _, result = pcall(function()
+                writefile(fileName, game:HttpGet(url))
+                return getcustomasset(fileName, true)
+            end)
+            if isfile(fileName) then
+                delfile(fileName)
+            end
             return result
         elseif isfile(url) then
             return getcustomasset(url, true)
@@ -49,10 +60,10 @@ Module.LoadCustomAsset = function(url: string): string?
 end
 
 Module.LoadCustomInstance = function(url: string): Instance?
-    local s, r = pcall(function()
+    local success, result = pcall(function()
         return game:GetObjects(Module.LoadCustomAsset(url))[1]
     end)
-    return s and r or nil
+    return success and result or nil
 end
 
 Module.GetGameLastUpdate = function(): DateTime
@@ -69,11 +80,11 @@ end
 
 Module.GetGitLastUpdate = function(owner: string, repo: string, filePath: string): DateTime
     local url = `https://api.github.com/repos/{owner}/{repo}/commits?per_page=1&path={filePath}`
-    local s, r = pcall(HttpService.JSONDecode, HttpService, game:HttpGet(url))
-    if not s then
+    local success, result = pcall(HttpService.JSONDecode, HttpService, game:HttpGet(url))
+    if not success then
         error(debug.traceback("Failed to get last commit for:\n"..url))
     end
-    return DateTime.fromIsoDate(r[1].commit.committer.date)
+    return DateTime.fromIsoDate(result[1].commit.committer.date)
 end
 
 Module.HasGitUpdated = function(owner: string, repo: string, filePath: string, timestamp: string | number | DateTime): boolean
@@ -89,7 +100,6 @@ Module.TruncateNumber = function(num: number, decimals: number): number
     return num * shift // 1 / shift
 end
 
--- Main
 for name, func in next, Module do
     if typeof(func) == "function" then
         getgenv()[name] = func
